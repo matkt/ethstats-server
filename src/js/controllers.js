937c3ae67724baca1fe99b9ade5afdb01fe261ff
Karim TAAM <karim.t2am@gmail.com>
2020-12-09 11:06:51 +0100

    update ui

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -166,7 +166,7 @@ netStatsApp.controller('StatsCtrl', function($scope, $filter, $localStorage, soc
 
 				if( $scope.nodes.length > 0 )
 				{
-					toastr['success']("Got nodes list", "Got nodes!");
+					//toastr['success']("Got nodes list", "Got nodes!");
 
 					updateActiveNodes();
 				}
@@ -571,7 +571,7 @@ netStatsApp.controller('StatsCtrl', function($scope, $filter, $localStorage, soc
       var xmlHttp = new XMLHttpRequest();
       xmlHttp.open( "POST", "http://3.16.76.137:8545", false ); // false for synchronous request
       xmlHttp.send( "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false], \"id\":1}" );
-      $scope.bestStats.block.gasLimit = JSON.parse(xmlHttp.responseText).result.baseFee;
+      $scope.bestStats.block.gasLimit = weiToHumanFriendlyString(parseInt(JSON.parse(xmlHttp.responseText).result.baseFee, 16));
 		}
 	}
 
@@ -652,5 +652,60 @@ netStatsApp.controller('StatsCtrl', function($scope, $filter, $localStorage, soc
 			return obj.replace(/\< *\/* *script *>*/gi,'').replace(/javascript/gi,'');
 		} else
 			return obj;
+	}
+
+
+	const UNIT_WEI = 'wei';
+	const WEI_MULTIPLIER = 1;
+	const UNIT_GWEI = 'gwei';
+	const GWEI_MULTIPLIER = 1000000000;
+	const UNIT_ETHER = 'ether';
+	const ETH_MULTIPLIER = 1000000000000000000;
+
+	function toWei(value, unit) {
+	    switch (unit) {
+	        case UNIT_WEI:
+	            return value * WEI_MULTIPLIER;
+	        case UNIT_GWEI:
+	            return value * GWEI_MULTIPLIER;
+	        case UNIT_ETHER:
+	            return value * ETH_MULTIPLIER;
+	    }
+	}
+
+	export function convert(from, fromUnit, toUnit) {
+	    if (fromUnit === toUnit) {
+	        return from;
+	    }
+	    if (fromUnit === UNIT_WEI) {
+	        if (toUnit === UNIT_GWEI) {
+	            return from / GWEI_MULTIPLIER;
+	        } else if (toUnit === UNIT_ETHER) {
+	            return from / ETH_MULTIPLIER;
+	        }
+	    } else if (fromUnit === UNIT_GWEI) {
+	        if (toUnit === UNIT_WEI) {
+	            return from * GWEI_MULTIPLIER;
+	        } else if (toUnit === UNIT_ETHER) {
+	            return from / GWEI_MULTIPLIER;
+	        }
+	    } else if (fromUnit === UNIT_ETHER) {
+	        if (toUnit === UNIT_WEI) {
+	            return from * ETH_MULTIPLIER;
+	        } else if (toUnit === UNIT_GWEI) {
+	            return from * GWEI_MULTIPLIER;
+	        }
+	    }
+	    return from;
+	}
+
+	function weiToHumanFriendlyString(value) {
+	    if (value < 10000000) {
+	        return `${value} wei`;
+	    } else if (value < 100000000000) {
+	        return `${convert(value, 'wei', 'gwei')} gwei`;
+	    } else {
+	        return `${convert(value, 'wei', 'ether')} eth`;
+	    }
 	}
 });
